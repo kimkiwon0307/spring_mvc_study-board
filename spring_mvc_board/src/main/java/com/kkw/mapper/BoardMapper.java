717f15fe2aa58d6401fa02483d52f9ee1ebee820
kkwkkj@naver.com <kkwkkj@naver.com>
2022-05-16 19:57:04 +0900

    springMVC로 게시판 만들기

---
+++ spring_mvc_board/src/main/java/com/kkw/mapper/BoardMapper.java
@@ -0,0 +1,21 @@
+package com.kkw.mapper;
+
+import java.util.List;
+
+import com.kkw.domain.BoardVO;
+
+public interface BoardMapper {
+
+	public List<BoardVO> getList();
+	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete (Long bno);
+	
+	public int update (BoardVO board);
+	
+}
